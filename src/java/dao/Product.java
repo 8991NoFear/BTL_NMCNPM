8c11afa1c43c9dfbb8c0d66a3cff5be51b93ea07
8991NoFear <ledinhbinhit@gmail.com>
2019-12-06 22:23:12 +0700

    Thêm các file dao

---
+++ src/java/dao/Product.java
@@ -0,0 +1,63 @@
+package dao;
+
+public class Product {
+    private int productID;
+    private int categoryID;
+    private String name;
+    private float price;
+    private String description;
+    private String image;
+    
+    public Product(){
+        
+    }
+
+    public int getProductID() {
+        return productID;
+    }
+
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    public int getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(int categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+    
+}
---
+++ src/java/dao/User.java
@@ -0,0 +1,54 @@
+package dao;
+
+public class User {
+    private int userID;
+    private String name;
+    private String email;
+    private int phone;
+    private String address;
+    
+    public User() {
+        
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public void setUserID(int userID) {
+        this.userID = userID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+}
